05af17b21b0ed892c6df4057109b44a4bfd2c650
Thanos Patapis <atpatapis@gmail.com>
2019-04-07 17:16:34 +0300

    Initial commit

---
+++ app/src/main/java/di/smartliving/app/StorageUnitsActivity.java
@@ -0,0 +1,13 @@
+package di.smartliving.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StorageUnitsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_storage_units);
+    }
+}
