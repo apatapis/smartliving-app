71da216326d10c3cd63839e95d9e574ef06b5bdf
Nevir <kounavi_8@hotmail.com>
2019-04-07 17:38:17 +0300

    Add Storage Unit Activity

---
+++ app/src/main/java/di/smartliving/app/StorageUnitActivity.java
@@ -0,0 +1,13 @@
+package di.smartliving.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StorageUnitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_storage_unit);
+    }
+}
